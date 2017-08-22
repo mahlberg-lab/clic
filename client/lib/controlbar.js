88c470a87e395136b8cfbabbce908870c722ef5f
Jamie Lentin <jm@lentin.co.uk>
2017-08-22 12:41:25 +0000

    client/controlbar: Use a common loaded event in index.js

--- client/lib/controlbar.js
+++ client/lib/controlbar.js
@@ -5,7 +5,7 @@ var jQuery = require('jquery/dist/jquery.slim.js');
 global.jQuery = jQuery;  // So chosen-js can find it
 var chosen = require('chosen-js');
 
-function controlbar_init(control_bar) {
+module.exports.init = function init(control_bar) {
     control_bar.addEventListener('click', function (e) {
         var i;
 
@@ -44,10 +44,4 @@ function controlbar_init(control_bar) {
     Array.prototype.forEach.call(control_bar.querySelectorAll('.chosen-select'), function (el, i) {
         jQuery(el).chosen();
     });
-}
-
-if (window) {
-    document.addEventListener('DOMContentLoaded', function (e) {
-        controlbar_init(document.getElementById('control-bar'));
-    });
-}
+};
--- client/lib/index.js
+++ client/lib/index.js
@@ -1,4 +1,13 @@
 "use strict";
 /*jslint todo: true, regexp: true, browser: true, unparam: true */
 /*global Promise */
-require('./controlbar.js');
+var controlbar = require('./controlbar.js');
+
+function page_load(e) {
+    controlbar.init(document.getElementById('control-bar'));
+}
+
+if (window) {
+    document.addEventListener('DOMContentLoaded', page_load);
+    window.addEventListener('popstate', page_load);
+}
