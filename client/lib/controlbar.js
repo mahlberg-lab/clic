8bc8aab397576e8056091406d67061faadadb4d1
Jamie Lentin <jm@lentin.co.uk>
2017-08-24 16:30:59 +0000

    client/page_concordance.js: Add existing options before updating form, so we keep KWIC terms on refresh

--- client/lib/controlbar.js
+++ client/lib/controlbar.js
@@ -26,6 +26,21 @@ var noUiSlider_opts = {
     },
 };
 
+function to_options_html(opts) {
+    function escapeHtml(s) {
+        // https://bugs.jquery.com/ticket/11773
+        return (String(s)
+            .replace(/&(?!\w+;)/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')); // "
+    }
+
+    return opts.map(function (t) {
+        return "<option>" + escapeHtml(t) + "</option>";
+    }).join("");
+}
+
 function ControlBar(control_bar) {
     this.control_bar = control_bar;
 
@@ -114,11 +129,14 @@ function ControlBar(control_bar) {
 
 // Refresh controls based on page_opts
 ControlBar.prototype.reload = function reload(page_opts) {
+    // Make sure we consider existing options valid
+    this.control_bar.elements['kwic-terms'].innerHTML = to_options_html([page_opts['kwic-terms']]);
+
     this.control_bar.elements['conc-subset'].value = page_opts['conc-subset'] || "all";
     this.control_bar.elements['conc-q'].value = page_opts['conc-q'] || "";
     this.control_bar.elements['conc-type'].value = page_opts['conc-type'] || "whole";
     this.control_bar.elements['kwic-span'].value = page_opts['kwic-span'] || "-5:5";
-    this.control_bar.elements['kwic-terms'].value = page_opts['kwic-terms'];
+    this.control_bar.elements['kwic-terms'].value = page_opts['kwic-terms'] || [];
 
     // Tell all the chosen's that values are altered
     Array.prototype.forEach.call(this.control_bar.querySelectorAll('.chosen-select'), function (el, i) {
@@ -130,15 +148,6 @@ ControlBar.prototype.reload = function reload(page_opts) {
 ControlBar.prototype.new_data = function new_data(data) {
     var prevVal, el;
 
-    function escapeHtml(s) {
-        // https://bugs.jquery.com/ticket/11773
-        return (String(s)
-            .replace(/&(?!\w+;)/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')); // "
-    }
-
     if (data.allWords) {
         // Make sure values already selected stay selectable
         el = this.control_bar.elements['kwic-terms'];
@@ -147,9 +156,9 @@ ControlBar.prototype.new_data = function new_data(data) {
             data.allWords[t] = true;
         });
 
-        jQuery(el).html(Object.keys(data.allWords).sort().map(function (t) {
-            return "<option>" + escapeHtml(t) + "</option>";
-        }).join("")).val(prevVal).trigger("chosen:updated");
+        el.innerHTML = to_options_html(Object.keys(data.allWords));
+        el.value = prevVal;
+        jQuery(el).trigger("chosen:updated");
     }
 
     this.control_bar.querySelectorAll("#kwic-total-matches")[0].innerText = (data.totalMatches || 0);
