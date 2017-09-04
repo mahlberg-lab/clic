c902274924f50e2880a5c49a44eeacd788ca678c
Jamie Lentin <jm@lentin.co.uk>
2017-09-04 16:03:02 +0000

    annotate: Ditch temporary output scripts to simplify script

--- annotation/annotate.sh
+++ annotation/annotate.sh
@@ -34,40 +34,29 @@ for i in $( ls | grep ".txt" ); do
 	nf=$i
 	nf=${nf/.txt/.xml}
 
-	echo 'Stage 1a -- basic paragraph extraction'
-	${PYTHON} $SCRIPT_DIR/paragraphs.py $i > $SCRIPT_DIR/tmp-paragraphs-1.xml
+	echo "Stage 1a -- basic paragraph extraction: $OUTPUT_DIR/tmp-paragraphs/$nf"
+	${PYTHON} $SCRIPT_DIR/paragraphs.py $i > $OUTPUT_DIR/tmp-paragraphs/$nf
 
-	echo 'Stage 1b -- numbering paragraphs and finding parts of the title'
-	${PYTHON} $SCRIPT_DIR/paragraphs_find_extra_chapter_titles.py $SCRIPT_DIR/tmp-paragraphs-1.xml > $SCRIPT_DIR/tmp-paragraphs-2.xml
-	cp $SCRIPT_DIR/tmp-paragraphs-2.xml $OUTPUT_DIR/paragraphs/$nf
+	echo "Stage 1b -- numbering paragraphs and finding parts of the title: $OUTPUT_DIR/paragraphs/$nf"
+	${PYTHON} $SCRIPT_DIR/paragraphs_find_extra_chapter_titles.py $OUTPUT_DIR/tmp-paragraphs/$nf > $OUTPUT_DIR/paragraphs/$nf
 
-	echo 'Stage 2 -- extracting sentences'
+	echo "Stage 2 -- extracting sentences: $OUTPUT_DIR/sentences/$nf"
+	${PYTHON} $SCRIPT_DIR/sentences.py $OUTPUT_DIR/paragraphs/$nf > $OUTPUT_DIR/sentences/$nf
 
-	${PYTHON} $SCRIPT_DIR/sentences.py $SCRIPT_DIR/tmp-paragraphs-2.xml > $SCRIPT_DIR/tmp-sentences.xml
-	cp $SCRIPT_DIR/tmp-sentences.xml $OUTPUT_DIR/sentences/$nf
+	echo "Stage 3 -- adding milestones for quotes: $OUTPUT_DIR/quotes/$nf"
+	${PYTHON} $SCRIPT_DIR/quotes.py $OUTPUT_DIR/sentences/$nf > $OUTPUT_DIR/quotes/$nf
 
-	echo 'Stage 3 -- adding milestones for quotes'
+	echo "Stage 4 -- adding milestones for suspensions: $OUTPUT_DIR/suspensions/$nf"
+	${PYTHON} $SCRIPT_DIR/suspensions.py $OUTPUT_DIR/quotes/$nf > $OUTPUT_DIR/suspensions/$nf
 
-	${PYTHON} $SCRIPT_DIR/quotes.py $SCRIPT_DIR/tmp-sentences.xml > $SCRIPT_DIR/tmp-quotes.xml
-	cp $SCRIPT_DIR/tmp-quotes.xml $OUTPUT_DIR/quotes/$nf
+	echo "Stage 5 -- adding milestones for alternative quotes: $OUTPUT_DIR/alternativequotes/$nf"
+	${PYTHON} $SCRIPT_DIR/alternativequotes.py $OUTPUT_DIR/suspensions/$nf > $OUTPUT_DIR/alternativequotes/$nf
 
-	echo 'Stage 4 -- adding milestones for suspensions'
+	echo "Stage 6 -- adding milestones for alternative suspensions: $OUTPUT_DIR/alternativesuspensions/$nf"
+	${PYTHON} $SCRIPT_DIR/alternativesuspensions.py $OUTPUT_DIR/alternativequotes/$nf > $OUTPUT_DIR/alternativesuspensions/$nf
 
-	${PYTHON} $SCRIPT_DIR/suspensions.py $SCRIPT_DIR/tmp-quotes.xml > $SCRIPT_DIR/tmp-suspensions.xml
-	cp $SCRIPT_DIR/tmp-suspensions.xml $OUTPUT_DIR/suspensions/$nf
-
-	echo 'Stage 5 -- adding milestones for alternative quotes'
-
-	${PYTHON} $SCRIPT_DIR/alternativequotes.py $SCRIPT_DIR/tmp-suspensions.xml > $SCRIPT_DIR/tmp-alternativequotes.xml
-	cp $SCRIPT_DIR/tmp-alternativequotes.xml $OUTPUT_DIR/alternativequotes/$nf
-
-	echo 'Stage 6 -- adding milestones for alternative suspensions'
-
-	${PYTHON} $SCRIPT_DIR/alternativesuspensions.py $SCRIPT_DIR/tmp-alternativequotes.xml > $SCRIPT_DIR/tmp-alternativesuspensions.xml
-	cp $SCRIPT_DIR/tmp-alternativesuspensions.xml $OUTPUT_DIR/alternativesuspensions/$nf
-
-	echo 'Writing the resuls to final, with stylesheet declaration'
-	echo '<?xml-stylesheet href="/styles.css"?>' | cat - $SCRIPT_DIR/tmp-alternativesuspensions.xml > $OUTPUT_DIR/final/$nf
+	echo "Final -- adding stylesheet declaration: $OUTPUT_DIR/final/$nf"
+	echo '<?xml-stylesheet href="/styles.css"?>' | cat - $OUTPUT_DIR/alternativesuspensions/$nf > $OUTPUT_DIR/final/$nf
 done
 
 echo 'Finished and now cleaning up. Find your results in the directory `final` in your output directory.'
