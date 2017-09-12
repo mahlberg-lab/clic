852458d7124527c8a129c9c09799190665559adf
Jamie Lentin <jm@lentin.co.uk>
2017-09-12 17:16:03 +0000

    client/api: Parse responses from XML endpoints

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,6 +1,6 @@
 "use strict";
 /*jslint todo: true, regexp: true, browser: true, unparam: true, plusplus: true */
-/*global Promise */
+/*global Promise, DOMParser */
 
 function to_query_string(params) {
     if (!params) {
@@ -17,10 +17,35 @@ function to_query_string(params) {
     }).join('&');
 }
 
+function parse_xml(str_promise, content_type) {
+    var parser = new DOMParser();
+
+    return str_promise.then(function (str) {
+        var doc = parser.parseFromString(str, content_type);
+
+        if (doc.documentElement.nodeName === "parsererror") {
+            throw new Error("Cannot parse XML document: " + str);
+        }
+        return doc;
+    });
+}
+
 module.exports.get = function (endpoint, qs) {
-    return window.fetch('/api/' + endpoint + '?' + to_query_string(qs)).then(function (response) {
+    var opts = {
+        headers: {
+            'Accept': 'application/json, application/xml',
+        },
+    };
+
+    return window.fetch('/api/' + endpoint + '?' + to_query_string(qs), opts).then(function (response) {
         if (response.status === 200) {
-            return response.json();
+            if (response.headers.get('Content-Type') === 'application/json') {
+                return response.json();
+            }
+            if (response.headers.get('Content-Type') === 'application/xml') {
+                return parse_xml(response.text(), response.headers.get('Content-Type'));
+            }
+            return { data: response.text() };
         }
 
         // It's an error, try and parse it, but throw an error in either case
