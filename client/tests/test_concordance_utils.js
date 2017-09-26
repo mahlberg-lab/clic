422989d8c26f5a23d0a6884fa0a61a9bab838e71
Jamie Lentin <jm@lentin.co.uk>
2017-09-26 15:55:21 +0000

    client/concordance_utils: Tests for renderTokenArray

---
+++ client/tests/test_concordance_utils.js
@@ -0,0 +1,57 @@
+"use strict";
+/*jslint todo: true, regexp: true, nomen: true, browser: true */
+var test = require('tape');
+
+var concordance_utils = require('../lib/concordance_utils.js');
+
+test('renderTokenArray:display', function (t) {
+    var data;
+
+    data = ['and', ' ', 'the', ' ', 'hammerhead', '-', 'sh<a>rk', ' ', 'said', ',', ' ', '"', [0, 2, 4, 6, 8]];
+    data.matches = [];
+    data.kwicSpan = {};
+
+    // Words are marks, space is span, HTML is quoted
+    t.equal(concordance_utils.renderTokenArray(data, 'display'),
+            '<div class="m0 l">' +
+            '<mark>and</mark>' +
+            '<span> </span>' +
+            '<mark>the</mark>' +
+            '<span> </span>' +
+            '<mark>hammerhead</mark>' +
+            '<span>-</span>' +
+            '<mark>sh&lt;a&gt;rk</mark>' +
+            '<span> </span>' +
+            '<mark>said</mark>' +
+            '<span>,</span>' +
+            '<span> </span>' +
+            '<span>&quot;</span>' +
+            '</div>');
+
+    // Reverse mode just changes the div class
+    data.kwicSpan = { reverse: true };
+    t.equal(concordance_utils.renderTokenArray(data, 'display'),
+            '<div class="m0 r">' +
+            '<mark>and</mark>' +
+            '<span> </span>' +
+            '<mark>the</mark>' +
+            '<span> </span>' +
+            '<mark>hammerhead</mark>' +
+            '<span>-</span>' +
+            '<mark>sh&lt;a&gt;rk</mark>' +
+            '<span> </span>' +
+            '<mark>said</mark>' +
+            '<span>,</span>' +
+            '<span> </span>' +
+            '<span>&quot;</span>' +
+            '</div>');
+
+    // Matches get added to class
+    data.matches = [1, 3];
+    t.equal(concordance_utils.renderTokenArray(data, 'display').replace(/>.*/, ">"), '<div class="m1 r">');
+    data.kwicSpan = {};
+    t.equal(concordance_utils.renderTokenArray(data, 'display').replace(/>.*/, ">"), '<div class="m1 l">');
+
+
+    t.end();
+});
