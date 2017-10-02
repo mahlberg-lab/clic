c23ef7e93405a06467d09d8f5732364966beec19
Jamie Lentin <jm@lentin.co.uk>
2017-10-02 11:36:29 +0000

    server/subset: Stripping first dict off results each time is silly, do it once

--- server/tests/test_subset.py
+++ server/tests/test_subset.py
@@ -24,44 +24,44 @@ class TestSubset(unittest.TestCase):
             corpora=['AgnesG'],
             subset=['longsus'],
             contextsize=[0],
-        )]
+        )][1:]
         out_context3 = [x for x in subset(
             cdb,
             corpora=['AgnesG'],
             subset=['longsus'],
             contextsize=[3],
-        )]
+        )][1:]
         out_context5 = [x for x in subset(
             cdb,
             corpora=['AgnesG'],
             subset=['longsus'],
             contextsize=[5],
-        )]
+        )][1:]
 
         # We don't include empty context columns
         self.assertEqual(
-            set(["".join(l[0][:-1]) for l in out_nocontext[1:]]),
-            set(["".join(l[1][:-1]) for l in out_context3[1:]]),
+            set(["".join(l[0][:-1]) for l in out_nocontext]),
+            set(["".join(l[1][:-1]) for l in out_context3]),
         )
         self.assertEqual(
-            set(["".join(l[1][:-1]) for l in out_context3[1:]]),
-            set(["".join(l[1][:-1]) for l in out_context5[1:]]),
+            set(["".join(l[1][:-1]) for l in out_context3]),
+            set(["".join(l[1][:-1]) for l in out_context5]),
         )
 
         # Context length is configurable
         self.assertEqual(
-            set([len(only_word(l[0])) for l in out_context3[1:]]),
+            set([len(only_word(l[0])) for l in out_context3]),
             set([3]),
         )
         self.assertEqual(
-            set([len(only_word(l[2])) for l in out_context3[1:]]),
+            set([len(only_word(l[2])) for l in out_context3]),
             set([3]),
         )
         self.assertEqual(
-            set([len(only_word(l[0])) for l in out_context5[1:]]),
+            set([len(only_word(l[0])) for l in out_context5]),
             set([3, 5]),  # NB: One of the subsets is at the end of the chapter
         )
         self.assertEqual(
-            set([len(only_word(l[2])) for l in out_context5[1:]]),
+            set([len(only_word(l[2])) for l in out_context5]),
             set([5]),
         )
