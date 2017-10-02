3afa4d42cca5b758cb3d0ae1922fd22b1f2f82da
Jamie Lentin <jm@lentin.co.uk>
2017-10-02 11:11:32 +0000

    server/web: Update stream_json's tests to match reality

--- server/tests/test_web.py
+++ server/tests/test_web.py
@@ -23,13 +23,13 @@ class Test_stream_json(unittest.TestCase):
         """Returning no values results in an empty array"""
         def fn():
             yield {}
-        self.assertEqual(self.sj(fn()), '{"data":[\r]}')
+        self.assertEqual(self.sj(fn()), '{"data":[\r\n]}')
 
     def test_headerandempty(self):
         """Just a header works"""
         def fn():
             yield {"moo": "yes"}
-        self.assertEqual(self.sj(fn()), '{"moo":"yes","data":[\r]}')
+        self.assertEqual(self.sj(fn()), '{"moo":"yes","data":[\r\n]}')
 
     def test_emptyheader(self):
         """An empty header doesn't trip us up"""
@@ -38,7 +38,7 @@ class Test_stream_json(unittest.TestCase):
             yield 1
             yield 2
             yield 3
-        self.assertEqual(self.sj(fn()), '{"data":[\r\n1\r,\n2\r,\n3\r]}')
+        self.assertEqual(self.sj(fn()), '{"data":[\r\n1\r,\n2\r,\n3\r\n]}')
 
     def test_headerresults(self):
         """All header items come before the results"""
@@ -47,4 +47,4 @@ class Test_stream_json(unittest.TestCase):
             yield 1
             yield 2
             yield 3
-        self.assertEqual(self.sj(fn()), '{"a":1,"b":2,"data":[\r\n1\r,\n2\r,\n3\r]}')
+        self.assertEqual(self.sj(fn()), '{"a":1,"b":2,"data":[\r\n1\r,\n2\r,\n3\r\n]}')
