99bd83ff7f531b104217efffbb6e9fd1428cccb8
Jamie Lentin <jm@lentin.co.uk>
2017-10-09 10:46:48 +0000

    client/page_contents: Neaten contents view

--- client/lib/page_contents.js
+++ client/lib/page_contents.js
@@ -32,14 +32,14 @@ function renderChapters(items) {
 
 function renderBooks(items) {
     return items.map(function (i) {
-        return "<h3>" + escapeHTML(i.title) + ": <small>" + renderSubsetInfo(i.subset_info) + "</small></h3>" +
+        return "<h4>" + escapeHTML(i.title) + ": <small>" + renderSubsetInfo(i.subset_info) + "</small></h3>" +
             renderChapters(i.children || []);
     }).join("\n");
 }
 
 function renderCorpora(items) {
     return items.map(function (i) {
-        return "<h2>" + escapeHTML(i.title) + ": <small>" + renderSubsetInfo(i.subset_info) + "</small></h2>" +
+        return "<h3>" + escapeHTML(i.title) + ": <small>" + renderSubsetInfo(i.subset_info) + "</small></h2>" +
             renderBooks(i.children || []);
     }).join("\n");
 }
@@ -52,7 +52,11 @@ PageContents.prototype.reload = function reload(page_state) {
     var self = this;
 
     return api.get('corpora/details').then(function (details) {
-        self.content_el.innerHTML = renderCorpora(details.corpora);
+        self.content_el.innerHTML = '<div class="clic-contents">' +
+           '<p>Welcome to CLiC. Please choose a function in the control bar to the right (click the icon in the top right if it is not displayed).</p>' +
+           '<p>CLiC currently contains the following texts:</p><div>' +
+           renderCorpora(details.corpora) +
+           '</div></div>';
     });
 
 };
