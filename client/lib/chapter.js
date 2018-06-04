61a76a8e976f35b719ebb78fee4379eed835a3a7
Jamie Lentin <jm@lentin.co.uk>
2018-06-04 15:45:47 +0000

    client/chapter: Move page component to it's own file, as-per others

--- client/lib/chapter.js
+++ client/lib/chapter.js
@@ -3,9 +3,8 @@
 /*global Promise */
 var ControlBar = require('./controlbar.js');
 var Analytics = require('./analytics.js');
-var api = require('./api.js');
 var PagePromise = require('./page_promise.js');
-var DisplayError = require('./alerts.js').prototype.DisplayError;
+var Chapter = require('./page_chapter.js');
 
 var state_defaults = {
     'start': 0,
@@ -17,74 +16,6 @@ var state_defaults = {
 };
 
 var page, cb, ga;
-
-function Chapter(content_el) {
-    /**
-      * Highlight terms between start and end, if args provided
-      */
-    this.highlight = function highlight(page_opts) {
-        var start_node = parseInt(page_opts.arg('start'), 10),
-            end_node = parseInt(page_opts.arg('end'), 10),
-            word_nodes,
-            i;
-
-        if (start_node === 0 && end_node === 0) {
-            return;
-        }
-        word_nodes = content_el.getElementsByTagName("w");
-        if (start_node >= word_nodes.length || end_node >= word_nodes.length) {
-            // Selection falls outside our range
-            return;
-        }
-        for (i = start_node; i < end_node; i++) {
-            word_nodes[i].setAttribute('selected', 'selected');
-        }
-        document.getElementById('scrollable-body').scrollTo(
-            0,
-            word_nodes[start_node].getBoundingClientRect().top
-                + window.pageYOffset
-                - (window.innerHeight / 2)
-        );
-    };
-
-    /**
-      * Load the given chapter and add to page
-      */
-    this.reload = function reload(page_opts) {
-        var self = this;
-
-        content_el.innerHTML = '';
-        return Promise.resolve().then(function () {
-            var args;
-
-            if (page_opts.arg('chapter_id') > -1) {
-                args = {
-                    chapter_id: page_opts.arg('chapter_id'),
-                };
-            } else {
-                if (!page_opts.arg('book')) {
-                    throw new DisplayError("Please select a book", "warn");
-                }
-                args = {
-                    book: page_opts.arg('book'),
-                    chapter_num: page_opts.arg('chapter_num'),
-                };
-            }
-
-            // Add a highlight-class for each specified highlight
-            content_el.className = page_opts.arg('chap-highlight').map(function (x) { return 'highlight-' + x; }).join(" ");
-
-            return api.get('chapter', args);
-        }).then(function (doc) {
-            content_el.appendChild(doc.documentElement);
-            self.highlight(page_opts);
-        });
-    };
-
-    this.page_title = function () {
-        return "CLiC chapter view";
-    };
-}
 
 function select_components(page_state) {
     if (!page) {
