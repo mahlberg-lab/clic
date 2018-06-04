ce9ec5f1e57a3d6cbc8b9a75b32523237e2bc3bc
Jamie Lentin <jm@lentin.co.uk>
2018-06-04 16:32:05 +0000

    client/chapter: Remove child rows now we have the chapters tab

--- client/lib/page_count.js
+++ client/lib/page_count.js
@@ -38,60 +38,13 @@ PageChapter.prototype.page_title = function (page_state) {
     return "CLiC word counts";
 };
 
-/** Do tweak after a reload, so we open any initially-selected rows */
-PageChapter.prototype.reload = function reload(page_state) {
-    var self = this;
-
-    return PageTable.prototype.reload.apply(this, arguments).then(function (data) {
-        return self.tweak(page_state).then(function () {
-            return data;
-        });
-    });
-};
-
-/** On tweak make sure the selected row is open */
-PageChapter.prototype.tweak = function tweak(page_state) {
-    var self = this;
-
-    return PageTable.prototype.tweak.apply(this, arguments).then(function (data) {
-        // Hide any rows that aren't selected any more
-        self.table.rows('.open').every(function () {
-            if (page_state.state('selected_rows').indexOf(this.id()) === -1) {
-                this.child.hide();
-                this.node().classList.remove('open');
-            }
-        });
-
-        self.table.rows(page_state.state('selected_rows').map(function (x) { return "#" + x; })).every(function () {
-            var book_id = this.data()[0];
-
-            if (this.node().classList.contains('open')) {
-                return;
-            }
-
-            this.node().classList.add('open');
-            this.child('<div>View chapter: ' + this.data().chapters.map(function (ch_num) {
-                if (ch_num === '_end') {
-                    return '';
-                }
-                return '<a target="_blank" href="/chapter?book=' + book_id + '&chapter_num=' + ch_num + '">' + ch_num + '</a>';
-            }).join(", &nbsp;") + '</div>', "child").show();
-
-            // Allow DOM to catch up, then expand it
-            window.setTimeout(function () {
-                this.child()[0].classList.add('expand');
-            }.bind(this), 10);
-        });
-    });
-};
-
 PageChapter.prototype.reload_data = function reload(page_state) {
     var api_opts = {};
 
     // Mangle page_state into the API's required parameters
     api_opts.corpora = page_state.arg('corpora');
     api_opts.subset = ['all', 'quote', 'nonquote', 'shortsus', 'longsus'];
-    api_opts.metadata = ['book_titles', 'chapter_start'];
+    api_opts.metadata = ['book_titles'];
 
     if (api_opts.corpora.length === 0) {
         throw new DisplayError("Please select the corpora to search in", "warn");
@@ -114,9 +67,6 @@ PageChapter.prototype.post_process = function (page_state, raw_data) {
         totals[2] += data[i][3];
         totals[3] += data[i][4];
         totals[4] += data[i][5];
-
-        // Add chapter counts for book
-        data[i].chapters = Object.keys(raw_data.chapter_start[data[i][0]] || {});
     }
 
     // Save book_titles ready for the renderer function
