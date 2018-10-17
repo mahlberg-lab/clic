62ba5198ab626d888276a1c56f113ce721a32771
Jamie Lentin <jm@lentin.co.uk>
2018-10-17 11:14:35 +0100

    server/db/cursor: Add logging mechanisms

--- server/clic/db/cursor.py
+++ server/clic/db/cursor.py
@@ -1,10 +1,27 @@
 import contextlib
 import os
+import logging
 
 import psycopg2
 from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import LoggingConnection as BaseLoggingConnection
 
 _pool = None
+
+logger = logging.getLogger(__name__)
+explain_logger = logging.getLogger(__name__ + '.explain')
+
+
+class LoggingConnection(BaseLoggingConnection):
+    def filter(self, msg, cur):
+        msg = msg.decode('utf8')  # String so loggers know how to print it
+        if explain_logger.isEnabledFor(logging.DEBUG):
+            if msg.startswith("EXPLAIN "):
+                return None  # Avoid infinite recursion
+            explain_cur = cur.connection.cursor()
+            explain_cur.execute("EXPLAIN " + msg)
+            explain_logger.debug("\n" + "\n".join(x[0] for x in explain_cur.fetchall()))
+        return msg
 
 
 @contextlib.contextmanager
@@ -12,9 +29,10 @@ def get_pool_cursor():
     global _pool
     dsn = os.environ['DB_DSN']
     if not _pool or dsn != _pool._kwargs['dsn']:
-        _pool = ThreadedConnectionPool(1, 10, dsn=dsn)
+        _pool = ThreadedConnectionPool(1, 10, dsn=dsn, connection_factory=LoggingConnection)
     conn = _pool.getconn()
     try:
+        conn.initialize(logger)
         yield conn.cursor()
         conn.commit()
     finally:
@@ -26,7 +44,8 @@ def get_pool_cursor():
 def get_script_cursor(for_write=False):
     """Return a single cursor for a short-lived script"""
     dsn = os.environ['DB_DSN']
-    conn = psycopg2.connect(dsn)
+    conn = psycopg2.connect(dsn, connection_factory=LoggingConnection)
+    conn.initialize(logger)
     conn.set_session(readonly=not(for_write))
     try:
         yield conn.cursor()
