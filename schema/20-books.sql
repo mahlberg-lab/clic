b73a777cfe861c43f9b8b7b04eba92e473f41153
Jamie Lentin <jm@lentin.co.uk>
2018-10-19 14:08:10 +0100

    schema/books: Break out helper functions into their own .sql file

--- schema/20-books.sql
+++ schema/20-books.sql
@@ -70,14 +70,6 @@ CREATE INDEX IF NOT EXISTS region_book_id_lower_crange ON region (book_id, LOWER
 COMMENT ON INDEX region_book_id_lower_crange IS 'Sorting on LOWER(crange)';
 
 
-CREATE OR REPLACE FUNCTION tokens_in_crange(in_book_id INT, in_crange INT4RANGE) RETURNS TABLE(crange INT4RANGE) STABLE AS
-$BODY$
-    SELECT t.crange
-      FROM token t
-      WHERE t.book_id = in_book_id AND t.crange <@ in_crange
-      ORDER BY LOWER(t.crange)
-$BODY$ LANGUAGE sql;
-COMMENT ON FUNCTION public.tokens_in_crange(book_id INT, crange INT4RANGE) IS 'Get all tokens within the given crange';
 
 
 DROP MATERIALIZED VIEW IF EXISTS book_metadata;
