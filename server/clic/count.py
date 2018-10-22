7886a0dd5940675f99761af7ac5c31b8622c7f24
Jamie Lentin <jm@lentin.co.uk>
2018-10-22 15:20:03 +0100

    server/count: Convert unittests to doctests

--- server/clic/count.py
+++ server/clic/count.py
@@ -21,6 +21,53 @@ Examples:
     {"data":[
         ["AgnesG",68197,21986]
     ], "version":{"corpora":"master:2affe56","clic:import":"1.6:876222b","clic":"1.7beta2:a1f93e7"}}
+
+Method
+------
+
+Word count peforms the following steps:
+
+1. Resolve the corpora option to a list of book IDs, translate the subset
+   selection to a database region.
+
+2. For each book, count the tokens that appear in each of the selected regions.
+
+Examples / edge cases
+---------------------
+
+These are the corpora we use for the following tests::
+
+    >>> db_cur = test_database(
+    ... alice='''
+    ... ‘Well!’ thought Alice to herself, ‘after such a fall as this, I shall
+    ... think nothing of tumbling down stairs! How brave they’ll all think me at
+    ... home! Why, I wouldn’t say anything about it, even if I fell off the top
+    ... of the house!’ (Which was very likely true.)
+    ... ''',
+    ...
+    ... willows='''
+    ... ‘Get off!’ spluttered the Rat, with his mouth full.
+    ...
+    ... ‘Thought I should find you here all right,’ said the Otter cheerfully.
+    ... ‘They were all in a great state of alarm along River Bank when I arrived
+    ... this morning.
+    ... ''')
+
+Get word counts::
+
+    >>> sorted(list(count(db_cur, ['alice', 'willows'], ['all'])))
+    [('alice', 49), ('willows', 38)]
+
+Get word counts / word counts in longsus. The result order is dictated by the arguments::
+
+    >>> sorted(list(count(db_cur, ['alice', 'willows'], ['all', 'longsus'])))
+    [('alice', 49, 0), ('willows', 38, 0)]
+
+    >>> sorted(list(count(db_cur, ['alice', 'willows'], ['longsus', 'all'])))
+    [('alice', 0, 49), ('willows', 0, 38)]
+
+TODO: Actual counts in quotes
+
 """
 from clic.db.book import get_book_metadata
 from clic.db.corpora import corpora_to_book_ids
