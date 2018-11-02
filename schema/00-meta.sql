728b04af4b1c00abe4bd6c6d7671a647aaf17b3f
Jamie Lentin <jm@lentin.co.uk>
2018-11-02 11:40:12 +0000

    .gitignore: Tidy up, move old entries to one side

--- schema/00-meta.sql
+++ schema/00-meta.sql
@@ -21,4 +21,4 @@ COMMIT;
 -- Before we begin, vacuum any existing data, creating the
 -- MATERIALISED VIEWS can be problematic otherwise.
 -- NB: This make take several minutes
-VACUUM ANALYSE;
+--VACUUM ANALYSE;
