c9226fcc00794d165f03618a793594073b6bd42e
Jamie Lentin <jm@lentin.co.uk>
2018-11-06 12:43:49 +0000

    server/db/lookup: book_id_name_lookup isn't used anywhere

--- server/clic/db/lookup.py
+++ server/clic/db/lookup.py
@@ -9,16 +9,6 @@ API_SUBSETS = dict(
 )
 
 
-def book_id_name_lookup(cur):
-    """
-    Query / return book_id -> short name lookup table. Cached against process
-    """
-    if 'book_id' not in _tables:
-        cur.execute("SELECT book_id, name FROM book")
-        _tables['book_id'] = dict(cur)
-    return _tables['book_id']
-
-
 def rclass_id_lookup(cur):
     """
     Return a lookup of rclass name -> rclass_id
