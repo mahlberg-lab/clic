b0b647b79e59d34e4b7e004a6b3a6a046e39dfb8
Jamie Lentin <jm@lentin.co.uk>
2018-11-16 15:46:51 +0000

    docs: Mock out all server dependencies instead of importing them
    Installing all dependencies takes ~yonks on readthedocs, so mock them instead.
    This also means that the install is more robust.

--- docs/conf.py
+++ docs/conf.py
@@ -175,3 +175,12 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+autodoc_mock_imports = """
+numpy pandas
+pybtex
+psycopg2
+icu
+unidecode
+flask flask_cors
+""".split()
