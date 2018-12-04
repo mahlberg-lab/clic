8fc2de37ea12b621c6c1f17a4bc49ecfb8d76440
Viola Wiegand <v.wiegand@bham.ac.uk>
2018-12-04 18:14:58 +0000

    server/region/quote: Don't include html in internal link

--- server/clic/region/quote.py
+++ server/clic/region/quote.py
@@ -222,7 +222,7 @@ quote.suspension regions
 ------------------------
 
 A suspension is defined as a ``quote.nonquote`` that is not bordered by the end or start
-of a sentence (see :mod:`clic.region.chapter.html`). 
+of a sentence (see :mod:`clic.region.chapter`). 
 If it is longer than 5 words, then it is a long suspension. For example::
 
     >>> [x for x in run_tagger('''
