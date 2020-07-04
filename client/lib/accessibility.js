c9498545093d1c79b40573522fc583ef7137e797
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-04 22:27:03 +0100

    general development

--- client/lib/accessibility.js
+++ client/lib/accessibility.js
@@ -1,13 +1,18 @@
 "use strict";
-var jQuery = require('jquery/dist/jquery.slim.js');
+// var jQuery = require('jquery/dist/jquery.slim.js');
 
 // Add accessibility attributes to the dynamically added content that has to be added retrospectively
 // (e.g. content inserted by a 3rd party library)
 module.exports.add_attributes = function () {
     // 'chosen-search-input' fields (added by chosen.js plugin)
-    jQuery('.chosen-search-input').each(function(index, object) {
-        jQuery(object).attr('title', 'chosen-search-input');
-        console.log(1);
-    });
+    // jQuery('.chosen-search-input').each(function(index, object) {
+    //     jQuery(object).attr('title', 'chosen-search-input');
+    //     console.log(1);
+    // });
+
+    var inputs = document.getElementsByClassName("chosen-search-input");
+    for (var i = 0; i < inputs.length; i++) {
+        inputs.item(i).setAttribute("class", "democlass"); 
+    }
 
 };
