aa52c7449d04830c6ef98d9d4db6c182440bb778
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-06 11:59:04 +0100

    general development

--- client/lib/cookies.js
+++ client/lib/cookies.js
@@ -1,6 +1,6 @@
 
 
-var getCookie = function (name) {
+function getCookie(name) {
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + name + "=");
 	if (parts.length == 2) return parts.pop().split(";").shift();
