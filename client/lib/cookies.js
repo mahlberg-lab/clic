185a71c7bfd5cd0cad0b7bb3a96f8123d7f9e73d
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-06 12:15:08 +0100

    general development

--- client/lib/cookies.js
+++ client/lib/cookies.js
@@ -1,15 +1,16 @@
-
+"use strict";
+/*jslint todo: true, regexp: true, browser: true, unparam: true, plusplus: true */
 
 function getCookie(name) {
-	var value = "; " + document.cookie;
-	var parts = value.split("; " + name + "=");
-	if (parts.length == 2) return parts.pop().split(";").shift();
+    var c = "; " + document.cookie.split("; " + name + "=");
+    if (c.length === 2) {
+        return c.pop().split(";").shift();
+    }
 }
 
 
 function x() {
     document.cookie = 'testCookie=testValue; expires=Fri, 31 Dec 2024 23:59:59 GMT';
-
     console.log(getCookie('test'));
 }
 
