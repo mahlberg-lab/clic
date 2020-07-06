436a84bdabb06267b2a26d925b658c12f1f79335
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-06 13:16:18 +0100

    general development

--- client/lib/cookies.js
+++ client/lib/cookies.js
@@ -2,19 +2,32 @@
 /*jslint todo: true, regexp: true, browser: true, unparam: true, plusplus: true */
 
 function getCookie(name) {
-    var c = "; " + document.cookie.split("; " + name + "=");
-    if (c.length === 2) {
-        return c.pop().split(";").shift();
-    }
 
-    console.log(c.length);
-    console.log(c);
+    // Convert cookies string to list
+    var c_list = document.cookie.split("; "),
+        i = 0,
+        c,
+        c_name,
+        c_value;
+    // Loop through cookies list to find a match
+    for (i = 0; i < c_list.length; i++) {
+        // Find cookie
+        c = c_list[i].split('=');
+        c_name = c[0];
+        c_value = c[1];
+        // Return cookie value if cookie name matches
+        if (c_name === name) {
+            return c_value;
+        }
+    }
+    // If no cookie found with given name, return null
+    return null;
 }
 
 
 function x() {
     document.cookie = 'testCookie=testValue; expires=Fri, 31 Dec 2024 23:59:59 GMT';
-    getCookie('test');
+    console.log(getCookie('test'));
 }
 
 module.exports = x;
