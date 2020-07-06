0e761bca66a59857ba5ec3c15752c9fe561922c3
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-06 11:44:02 +0100

    general development

---
+++ client/lib/cookies.js
@@ -0,0 +1,10 @@
+"use strict";
+/*jslint todo: true, regexp: true, browser: true, unparam: true, plusplus: true */
+/*global Promise */
+var jQuery = require('jquery/dist/jquery.slim.js');
+
+function x() {
+    console.log(1);
+}
+
+module.exports = x;
