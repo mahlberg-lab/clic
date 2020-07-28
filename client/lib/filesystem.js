60649d1eb51253efbb1689face88706457eb1da4
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-07-28 11:30:18 +0100

    fixed load bug, had to point to the input not the label

--- client/lib/filesystem.js
+++ client/lib/filesystem.js
@@ -113,7 +113,7 @@ module.exports.file_loader = function file_loader(document, fn) {
 
     // Create input element
     el.innerHTML = '<label for="file" style="display: none;">File system</label><input type="file" title="file" style="visibility: hidden; position: absolute; top: 0px; left: 0px; height: 0px; width: 0px;">';
-    el = el.children[0];
+    el = el.children[1];
     document.body.appendChild(el);
 
     // Attach events
