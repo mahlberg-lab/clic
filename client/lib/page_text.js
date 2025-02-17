c5ae421fa34f815f9f0c4d1fcae733d8f42403a1
Jamie Lentin <jm@lentin.co.uk>
2025-02-17 18:20:51 +0000

    client/page_text: Rework to avoid race conditions
    A race condition between chapter-sorting & highlight-sorting was
    occasionally causing page loads not on the highlighted word.
    
    The promise structure was needlessly complex anyway, so rewrite.
    
    * Move onscroll event outside main promise, possibly to make it a
    page_promise option
    * Collapse promise events together
    * Pick apart double-usage of force_update, just have rerendered
    deciding whether we should try to scroll to the highlight

--- client/lib/page_text.js
+++ client/lib/page_text.js
@@ -7,20 +7,71 @@ var DisplayError = require('./alerts.js').prototype.DisplayError;
 
 function PageText(content_el) {
     this.current = {};
+
+    /**
+      * Scroll event: Find which chapter user is looking at, and send tweak event
+      */
+    this.scroll = function () {
+        var title_els, body_el = document.getElementById('scrollable-body');
+
+        // Find all titles that are above the bottom of the page
+        title_els = Array.prototype.filter.call(document.querySelectorAll('#content .chapter-title'), function (el) {
+            return el.offsetParent && el.offsetTop < (el.offsetParent.scrollTop + el.offsetParent.offsetHeight);
+        });
+        if (title_els.length > 0) {
+            window.dispatchEvent(new window.CustomEvent('state_tweak', { detail: {
+                args: {
+                    chapter_num: [title_els[title_els.length - 1].className.match(/chapter-(\d+)/)[1]],
+                },
+                state: {
+                    "scroll-pos": body_el.scrollTop,
+                },
+            }}));
+        } else {
+            window.dispatchEvent(new window.CustomEvent('state_tweak', { detail: {
+                state: {
+                    "scroll-pos": body_el.scrollTop,
+                },
+            }}));
+        }
+    };
+
     /**
       * Load the given text and add to page
       */
     this.reload = function reload(page_state) {
-        var p = Promise.resolve({}), force_update = false;
+        // Hook into the scroll event, use it to keep the chapter_num parameter up-to-date
+        document.getElementById('scrollable-body').onscroll = function event_fn(e) {
+            var body_el = e.target;
 
-        if (JSON.stringify(page_state.arg('book')) !== this.current.book) {
-            p = p.then(function (p_data) {
-                var args;
+            if (body_el.scroll_timeout) {
+                // Clear any previous scroll timeouts
+                window.clearTimeout(body_el.scroll_timeout);
+            }
+            body_el.scroll_timeout = window.setTimeout(function () {
+                if (!body_el.querySelector(":scope > #content > .book-content")) {
+                    // Not part of the page anymore, so tidy up
+                    body_el.onscroll = undefined;
+                    return;
+                }
+                this.scroll();
+
+            }.bind(this), 300);
+        }.bind(this);
+
+        return Promise.resolve().then(function () {
+            var args;
+
+            if (!page_state.arg('book')) {
+                content_el.innerHTML = '';
+                throw new DisplayError("Please select a book", "warn");
+            }
+
+            if (JSON.stringify(page_state.arg('book')) !== this.current.book) {
+                // NB: Rebuild this.current to invalidate anything else stored in it
+                this.current = { book: JSON.stringify(page_state.arg('book')) };
 
                 content_el.innerHTML = '';
-                if (!page_state.arg('book')) {
-                    throw new DisplayError("Please select a book", "warn");
-                }
                 args = {
                     corpora: page_state.arg('book'),
                     regions: [
@@ -36,23 +87,18 @@ function PageText(content_el) {
                     ],
                 };
 
-                return api.get('text', args);
-            }.bind(this)).then(function (data) {
-                this.content = data.content;
-                this.regions = data.data;
+                // Fetch book text, stash in page object
+                return api.get('text', args).then(function (data) {
+                    this.content = data.content;
+                    this.regions = data.data;
+                }.bind(this));
+            }
+        }.bind(this)).then(function () {
+            var book_el, highlight_arr, rerendered = false;
 
-                data.chapter_nums = corpora_utils.chapter_headings(this.content, this.regions);
-
-                return data;
-            }.bind(this));
-            this.current.book = JSON.stringify(page_state.arg('book'));
-            force_update = true;
-        }
-
-        // Highlight any words in chapter_num (e.g. for concordance selection)
-        if (force_update || JSON.stringify(page_state.arg('word-highlight')) !== this.current['word-highlight']) {
-            p = p.then(function (p_data) {
-                var book_el, highlight_arr;
+            // Render book, highlighting any words in chapter_num (e.g. for concordance selection)
+            if (JSON.stringify(page_state.arg('word-highlight')) !== this.current['word-highlight']) {
+                this.current['word-highlight'] = JSON.stringify(page_state.arg('word-highlight'));
 
                 highlight_arr = page_state.arg('word-highlight').split(':').map(function (x) {
                     return parseInt(x, 10);
@@ -66,100 +112,38 @@ function PageText(content_el) {
                 book_el.className = 'book-content';
                 book_el.innerHTML = corpora_utils.regions_to_html(this.content, this.regions, highlight_arr);
                 content_el.appendChild(book_el);
-
-                // Hook into the scroll event, use it to keep the chapter_num parameter up-to-date
-                document.getElementById('scrollable-body').addEventListener('scroll', function event_fn(e) {
-                    if (book_el.scroll_timeout) {
-                        // Clear any previous scroll timeouts
-                        window.clearTimeout(book_el.scroll_timeout);
-                    }
-                    book_el.scroll_timeout = window.setTimeout(function () {
-                        var body_el = document.getElementById('scrollable-body'),
-                            title_els;
-
-                        if (!body_el.contains(book_el)) {
-                            // Not part of the page anymore, so tidy up
-                            body_el.removeEventListener('scroll', event_fn);
-                            return;
-                        }
-
-                        // Find all titles that are above the bottom of the page
-                        title_els = Array.prototype.filter.call(document.querySelectorAll('#content .chapter-title'), function (el) {
-                            return el.offsetParent && el.offsetTop < (el.offsetParent.scrollTop + el.offsetParent.offsetHeight);
-                        });
-                        if (title_els.length > 0) {
-                            window.dispatchEvent(new window.CustomEvent('state_tweak', { detail: {
-                                args: {
-                                    chapter_num: [title_els[title_els.length - 1].className.match(/chapter-(\d+)/)[1]],
-                                },
-                                state: {
-                                    "scroll-pos": body_el.scrollTop,
-                                },
-                            }}));
-                        } else {
-                            window.dispatchEvent(new window.CustomEvent('state_tweak', { detail: {
-                                state: {
-                                    "scroll-pos": body_el.scrollTop,
-                                },
-                            }}));
-                        }
-                    }, 300);
-                });
-
-                return p_data;
-            }.bind(this));
-            this.current['word-highlight'] = JSON.stringify(page_state.arg('word-highlight'));
-        }
-
-        if (force_update || JSON.stringify(page_state.arg('chapter_num')) !== this.current.chapter_num) {
-            p = p.then(function (p_data) {
-                // Find chapter to scroll to, or top of page
-                var chapter_el = content_el.querySelector(
-                    '.chapter-title.chapter-' + page_state.arg('chapter_num')
-                );
-
-                if (chapter_el) {
-                    // Tell controlbar about the changes
-                    p_data.chapter_num_selected = chapter_el.className.match(/chapter-(\d+)/)[1];
-                }
-
-                return p_data;
-            });
-            this.current.chapter_num = JSON.stringify(page_state.arg('chapter_num'));
-        }
-
-        if (force_update || JSON.stringify(page_state.arg('chap-highlight')) !== this.current['chap-highlight']) {
-            p = p.then(function (p_data) {
-                // Add a highlight-class for each specified highlight
-                content_el.childNodes[0].className = 'book-content ' +
-                    page_state.arg('chap-highlight').map(function (x) {
-                        return 'h-' + x.replace(/\./g, '-');
-                    }).join(" ");
-
-                return p_data;
-            });
-            this.current['chap-highlight'] = JSON.stringify(page_state.arg('chap-highlight'));
-        }
-
-        p = p.then(function (p_data) {
-            if (!force_update && p_data.chapter_num_selected) {
-                // chapter_num changed after page load, scroll to that (and ignore any scroll-pos)
-                content_el.querySelector('.book-content > .chapter-title.chapter-' + page_state.arg('chapter_num')).scrollIntoView();
-            } else if (page_state.state('scroll-pos') > -1) {
-                // Scroll position already set, scroll to that
-                document.getElementById('scrollable-body').scrollTop = page_state.state('scroll-pos');
-            } else if (page_state.arg('word-highlight') !== '0:0') {
-                // Fresh load from link with a highlight, scroll to that
-                content_el.querySelector('.book-content > .highlight').scrollIntoView();
-            } else if (page_state.arg('chapter_num') > 0) {
-                // Fresh load from link with chapter_num, scroll to that
-                content_el.querySelector('.book-content > .chapter-title.chapter-' + page_state.arg('chapter_num')).scrollIntoView();
+                rerendered = true;
             }
 
-            return p_data;
-        }.bind(this));
+            // Add a highlight-class for each specified highlight
+            content_el.childNodes[0].className = 'book-content ' + page_state.arg('chap-highlight').map(function (x) {
+                return 'h-' + x.replace(/\./g, '-');
+            }).join(" ");
 
-        return p;
+            if (rerendered) {
+                // Freshly loaded, try harder to find something to scroll to
+                // NB: Timeout to let content attach to page
+                window.setTimeout(function () {
+                    if (page_state.arg('word-highlight') !== '0:0') {
+                        content_el.querySelector('.book-content > .highlight').scrollIntoView({block: "center"});
+                    } else if (page_state.arg('chapter_num') > 0) {
+                        content_el.querySelector('.book-content > .chapter-title.chapter-' + page_state.arg('chapter_num')).scrollIntoView();
+                    } else if (page_state.state('scroll-pos') > -1) {
+                        document.getElementById('scrollable-body').scrollTop = page_state.state('scroll-pos');
+                    }
+                }, 100);
+            } else {
+                if (page_state.arg('chapter_num') > 0) {
+                    content_el.querySelector('.book-content > .chapter-title.chapter-' + page_state.arg('chapter_num')).scrollIntoView();
+                }
+            }
+
+            // Return data for ControlBar.prototype.new_data
+            return {
+                chapter_nums: corpora_utils.chapter_headings(this.content, this.regions),
+                chapter_num_selected: page_state.arg('chapter_num'),
+            };
+        }.bind(this));
     };
 
     this.tweak = function tweak(page_state) {
