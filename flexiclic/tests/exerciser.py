12e73e0c392c6f13211a1d86adfe5992b0544895
Jamie Lentin <jm@lentin.co.uk>
2025-02-26 16:36:22 +0000

    flexiclic/tests: Baseline test for algorithm->HTML
    Output HTML into a .ini baseline file, compare it to a baseline.

--- flexiclic/tests/exerciser.py
+++ flexiclic/tests/exerciser.py
@@ -13,8 +13,3 @@ for x in fc.compute_path(dict(
     #metadata=["book_titles", "chapter_start"],
 ), []):
     print(x)
-
-for algo_class, algos in fc.algorithms_by_class().items():
-    for a in algos:
-        print("\n\n==== %s:%s" % (algo_class, a['label']))
-        print("\n".join(fc.algorithm_render_html(algo_name=a['name'], prefix="algo")))
---
+++ flexiclic/tests/test_algo_html.py
@@ -0,0 +1,40 @@
+import configparser
+import io
+import unittest
+import warnings
+
+from flexiclic import FlexiClic
+
+
+class TestAlgoHtml(unittest.TestCase):
+    maxDiff = None
+
+    def test_from_schema_all_algos(self):
+        # Write all algo HTML to a configparser file
+        fc = FlexiClic(api_root="https://clic.bham.ac.uk")
+        actual = configparser.ConfigParser()
+        for algo_class, algos in fc.algorithms_by_class().items():
+            for a in algos:
+                actual['DEFAULT'][a['name']] = "\n" + "\n".join(fc.algorithm_render_html(algo_name=a['name'], prefix="algprefix"))
+        with open("tests/test_algo_html.baseline.new", "w", encoding="utf-8") as f:
+            actual.write(f)
+
+        # Read in both versions (NB: Re-reading as whitespace isn't preserved)
+        actual = configparser.ConfigParser()
+        actual.read("tests/test_algo_html.baseline.new")
+        expected = configparser.ConfigParser()
+        expected.read("tests/test_algo_html.baseline")
+
+        self.assertEqual(
+            actual['DEFAULT'].keys(),
+            expected['DEFAULT'].keys(),
+        )
+        for k in actual['DEFAULT'].keys():
+            for l in actual['DEFAULT'][k].split("\n"):
+                if "border: 1px solid red" in l:
+                    warnings.warn(l)
+            self.assertEqual(
+                actual['DEFAULT'][k],
+                expected['DEFAULT'][k],
+                k,
+            )
