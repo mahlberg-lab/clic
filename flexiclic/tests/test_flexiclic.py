2d9857751b495eac301b218708a28f57797ba204
Jamie Lentin <jm@lentin.co.uk>
2025-02-27 17:57:38 +0000

    test_flexiclic: Test term highlight support

--- flexiclic/tests/test_flexiclic.py
+++ flexiclic/tests/test_flexiclic.py
@@ -128,3 +128,26 @@ class TestFlexiClic(unittest.TestCase):
             (['Partition', []], ['hello', []], ["('to', 'else')", []], ['', '', ''], ['', '', ''], 5, '', {'rowcount': 1}),
             (3, 5, 3, {'matches': None}),
         ])
+
+    def test_compute_path_term_highlight(self):
+        data = [
+            [["by"," ","Temple"," ","Bar",", ","in"," ","Lincoln's"," ","Inn"," ","Hall",", ","at"," ","the"," ","very"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",2547,2552],[1,4,18]],
+            [["patience",", ","courage",", ","hope",", ","so"," ","overthrows"," ","the"," ","brain"," ","and"," ","breaks"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","that"," ","there"," ","is"," ","not"," ","an"," ","honourable"," ","man"," ","among"," ","its"," ","practitioners",[1,3,5,7,9,11,13,15,17,19]],["BH",5230,5235],[1,6,24]],
+            [["Thus",", ","in"," ","the"," ","midst"," ","of"," ","the"," ","mud"," ","and"," ","at"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",11558,11563],[1,11,59]],
+            [["to"," ","open"," ","my"," ","lips",", ","and"," ","never"," ","dared"," ","to"," ","open"," ","my"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","to"," ","anybody"," ","else",". ","It"," ","almost"," ","makes"," ","me"," ","cry"," ","to"," ","think",[1,3,5,7,9,11,13,15,17,19]],["BH",32184,32189],[3,2,6]],
+            [["used"," ","ardently"," ","to"," ","hope"," ","that"," ","I"," ","might"," ","have"," ","a"," ","better"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",";",[0]],[" ","and"," ","I"," ","talked"," ","it"," ","over"," ","very"," ","often"," ","with"," ","the"," ","dear",[1,3,5,7,9,11,13,15,17,19]],["BH",33835,33840],[3,3,20]],
+            [["she"," ","looked"," ","at"," ","me",", ","and"," ","laid"," ","it"," ","on"," ","my"," ","fluttering"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",".",[0]],[" ","She"," ","raised"," ","me",", ","sat"," ","in"," ","her"," ","chair",", ","and"," ","standing"," ","me",[1,3,5,7,9,11,13,15,17,19]],["BH",37435,37440],[3,14,58]],
+            [["I"," ","had"," ","brought"," ","no"," ","joy"," ","at"," ","any"," ","time"," ","to"," ","anybody's"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","and"," ","that"," ","I"," ","was"," ","to"," ","no"," ","one"," ","upon"," ","earth"," ","what",[1,3,5,7,9,11,13,15,17,19]],["BH",38895,38900],[3,16,69]],
+        ]
+        out = list(self._compute_path(data=data, path=[
+            { "algorithm_name": "KWIC Grouper Ranker", "count_types": "on", "search_term": "the", "tokens_attribute": "word", "window_end": "10", "window_start": "-10"},
+        ]))
+        self.assertEqual(out, [
+            (0, 0, 0, {'matches': [[2], [], []], 'rank_keys': {'algo_0': 1}}),
+            (1, 0, 1, {'matches': [[5], [], []], 'rank_keys': {'algo_0': 1}}),
+            (2, 0, 2, {'matches': [[8], [], []], 'rank_keys': {'algo_0': 1}}),
+            (4, 0, 4, {'matches': [[], [], [9]], 'rank_keys': {'algo_0': 1}}),
+            (3, 0, 3, {'matches': None, 'rank_keys': {'algo_0': 0}}),
+            (5, 0, 5, {'matches': None, 'rank_keys': {'algo_0': 0}}),
+            (6, 0, 6, {'matches': None, 'rank_keys': {'algo_0': 0}}),
+        ])
