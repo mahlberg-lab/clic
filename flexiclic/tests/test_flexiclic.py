7340e3df1b8ab999ac46dd6520dc15f0005ca9b5
Jamie Lentin <jm@lentin.co.uk>
2025-03-03 11:43:20 +0000

    test_flexiclic: Don't repeat concordance data
    It's all the same, no reason to copy-paste it.

--- flexiclic/tests/test_flexiclic.py
+++ flexiclic/tests/test_flexiclic.py
@@ -8,6 +8,16 @@ from flexiclic import FlexiClic
 class TestFlexiClic(unittest.TestCase):
     maxDiff = None
 
+    conc_data = [
+        [["by"," ","Temple"," ","Bar",", ","in"," ","Lincoln's"," ","Inn"," ","Hall",", ","at"," ","the"," ","very"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",2547,2552],[1,4,18]],
+        [["patience",", ","courage",", ","hope",", ","so"," ","overthrows"," ","the"," ","brain"," ","and"," ","breaks"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","that"," ","there"," ","is"," ","not"," ","an"," ","honourable"," ","man"," ","among"," ","its"," ","practitioners",[1,3,5,7,9,11,13,15,17,19]],["BH",5230,5235],[1,6,24]],
+        [["Thus",", ","in"," ","the"," ","midst"," ","of"," ","the"," ","mud"," ","and"," ","at"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",11558,11563],[1,11,59]],
+        [["to"," ","open"," ","my"," ","lips",", ","and"," ","never"," ","dared"," ","to"," ","open"," ","my"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","to"," ","anybody"," ","else",". ","It"," ","almost"," ","makes"," ","me"," ","cry"," ","to"," ","think",[1,3,5,7,9,11,13,15,17,19]],["BH",32184,32189],[3,2,6]],
+        [["used"," ","ardently"," ","to"," ","hope"," ","that"," ","I"," ","might"," ","have"," ","a"," ","better"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",";",[0]],[" ","and"," ","I"," ","talked"," ","it"," ","over"," ","very"," ","often"," ","with"," ","the"," ","dear",[1,3,5,7,9,11,13,15,17,19]],["BH",33835,33840],[3,3,20]],
+        [["she"," ","looked"," ","at"," ","me",", ","and"," ","laid"," ","it"," ","on"," ","my"," ","fluttering"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",".",[0]],[" ","She"," ","raised"," ","me",", ","sat"," ","in"," ","her"," ","chair",", ","and"," ","standing"," ","me",[1,3,5,7,9,11,13,15,17,19]],["BH",37435,37440],[3,14,58]],
+        [["I"," ","had"," ","brought"," ","no"," ","joy"," ","at"," ","any"," ","time"," ","to"," ","anybody's"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","and"," ","that"," ","I"," ","was"," ","to"," ","no"," ","one"," ","upon"," ","earth"," ","what",[1,3,5,7,9,11,13,15,17,19]],["BH",38895,38900],[3,16,69]],
+    ]
+
     def _compute_path(self, data=[], path=[]):
         query_opts = {
             "corpora": ["BH"],
@@ -57,17 +67,8 @@ class TestFlexiClic(unittest.TestCase):
                     yield out_l
 
     def test_compute_path_nopartition(self):
-        data = [
-            [["by"," ","Temple"," ","Bar",", ","in"," ","Lincoln's"," ","Inn"," ","Hall",", ","at"," ","the"," ","very"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",2547,2552],[1,4,18]],
-            [["patience",", ","courage",", ","hope",", ","so"," ","overthrows"," ","the"," ","brain"," ","and"," ","breaks"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","that"," ","there"," ","is"," ","not"," ","an"," ","honourable"," ","man"," ","among"," ","its"," ","practitioners",[1,3,5,7,9,11,13,15,17,19]],["BH",5230,5235],[1,6,24]],
-            [["Thus",", ","in"," ","the"," ","midst"," ","of"," ","the"," ","mud"," ","and"," ","at"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",11558,11563],[1,11,59]],
-            [["to"," ","open"," ","my"," ","lips",", ","and"," ","never"," ","dared"," ","to"," ","open"," ","my"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","to"," ","anybody"," ","else",". ","It"," ","almost"," ","makes"," ","me"," ","cry"," ","to"," ","think",[1,3,5,7,9,11,13,15,17,19]],["BH",32184,32189],[3,2,6]],
-            [["used"," ","ardently"," ","to"," ","hope"," ","that"," ","I"," ","might"," ","have"," ","a"," ","better"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",";",[0]],[" ","and"," ","I"," ","talked"," ","it"," ","over"," ","very"," ","often"," ","with"," ","the"," ","dear",[1,3,5,7,9,11,13,15,17,19]],["BH",33835,33840],[3,3,20]],
-            [["she"," ","looked"," ","at"," ","me",", ","and"," ","laid"," ","it"," ","on"," ","my"," ","fluttering"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",".",[0]],[" ","She"," ","raised"," ","me",", ","sat"," ","in"," ","her"," ","chair",", ","and"," ","standing"," ","me",[1,3,5,7,9,11,13,15,17,19]],["BH",37435,37440],[3,14,58]],
-            [["I"," ","had"," ","brought"," ","no"," ","joy"," ","at"," ","any"," ","time"," ","to"," ","anybody's"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","and"," ","that"," ","I"," ","was"," ","to"," ","no"," ","one"," ","upon"," ","earth"," ","what",[1,3,5,7,9,11,13,15,17,19]],["BH",38895,38900],[3,16,69]],
-        ]
         # No path, just get lines back in same order
-        out = list(self._compute_path(data=data, path=[
+        out = list(self._compute_path(data=self.conc_data, path=[
         ]))
         self.assertEqual(out, [
             (0, 0, 0, {'matches': None}),
@@ -80,7 +81,7 @@ class TestFlexiClic(unittest.TestCase):
         ])
 
         # Random sample, get given line
-        out = list(self._compute_path(data=data, path=[
+        out = list(self._compute_path(data=self.conc_data, path=[
             {"algorithm_name":"Random Sample","sample_size":"2","seed":"3"},
         ]))
         self.assertEqual(out, [
@@ -89,7 +90,7 @@ class TestFlexiClic(unittest.TestCase):
         ])
 
         # Random sample+sort
-        out = list(self._compute_path(data=data, path=[
+        out = list(self._compute_path(data=self.conc_data, path=[
             {"algorithm_name":"Random Sample","sample_size":"4","seed":"3"},
             {"algorithm_name":"Random Sort","seed":"3"},
         ]))
@@ -101,16 +102,7 @@ class TestFlexiClic(unittest.TestCase):
         ])
 
     def test_compute_path_partition(self):
-        data = [
-            [["by"," ","Temple"," ","Bar",", ","in"," ","Lincoln's"," ","Inn"," ","Hall",", ","at"," ","the"," ","very"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",2547,2552],[1,4,18]],
-            [["patience",", ","courage",", ","hope",", ","so"," ","overthrows"," ","the"," ","brain"," ","and"," ","breaks"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","that"," ","there"," ","is"," ","not"," ","an"," ","honourable"," ","man"," ","among"," ","its"," ","practitioners",[1,3,5,7,9,11,13,15,17,19]],["BH",5230,5235],[1,6,24]],
-            [["Thus",", ","in"," ","the"," ","midst"," ","of"," ","the"," ","mud"," ","and"," ","at"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",11558,11563],[1,11,59]],
-            [["to"," ","open"," ","my"," ","lips",", ","and"," ","never"," ","dared"," ","to"," ","open"," ","my"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","to"," ","anybody"," ","else",". ","It"," ","almost"," ","makes"," ","me"," ","cry"," ","to"," ","think",[1,3,5,7,9,11,13,15,17,19]],["BH",32184,32189],[3,2,6]],
-            [["used"," ","ardently"," ","to"," ","hope"," ","that"," ","I"," ","might"," ","have"," ","a"," ","better"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",";",[0]],[" ","and"," ","I"," ","talked"," ","it"," ","over"," ","very"," ","often"," ","with"," ","the"," ","dear",[1,3,5,7,9,11,13,15,17,19]],["BH",33835,33840],[3,3,20]],
-            [["she"," ","looked"," ","at"," ","me",", ","and"," ","laid"," ","it"," ","on"," ","my"," ","fluttering"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",".",[0]],[" ","She"," ","raised"," ","me",", ","sat"," ","in"," ","her"," ","chair",", ","and"," ","standing"," ","me",[1,3,5,7,9,11,13,15,17,19]],["BH",37435,37440],[3,14,58]],
-            [["I"," ","had"," ","brought"," ","no"," ","joy"," ","at"," ","any"," ","time"," ","to"," ","anybody's"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","and"," ","that"," ","I"," ","was"," ","to"," ","no"," ","one"," ","upon"," ","earth"," ","what",[1,3,5,7,9,11,13,15,17,19]],["BH",38895,38900],[3,16,69]],
-        ]
-        out = list(self._compute_path(data=data, path=[
+        out = list(self._compute_path(data=self.conc_data, path=[
             {"algorithm_name":"KWIC Patterns","positions":["1","3"],"tokens_attribute":"word"},
         ]))
         self.assertEqual(out, [
@@ -130,16 +122,7 @@ class TestFlexiClic(unittest.TestCase):
         ])
 
     def test_compute_path_term_highlight(self):
-        data = [
-            [["by"," ","Temple"," ","Bar",", ","in"," ","Lincoln's"," ","Inn"," ","Hall",", ","at"," ","the"," ","very"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",2547,2552],[1,4,18]],
-            [["patience",", ","courage",", ","hope",", ","so"," ","overthrows"," ","the"," ","brain"," ","and"," ","breaks"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","that"," ","there"," ","is"," ","not"," ","an"," ","honourable"," ","man"," ","among"," ","its"," ","practitioners",[1,3,5,7,9,11,13,15,17,19]],["BH",5230,5235],[1,6,24]],
-            [["Thus",", ","in"," ","the"," ","midst"," ","of"," ","the"," ","mud"," ","and"," ","at"," ","the"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","of"," ","the"," ","fog",", ","sits"," ","the"," ","Lord"," ","High"," ","Chancellor"," ","in"," ","his",[1,3,5,7,9,11,13,15,17,19]],["BH",11558,11563],[1,11,59]],
-            [["to"," ","open"," ","my"," ","lips",", ","and"," ","never"," ","dared"," ","to"," ","open"," ","my"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",",",[0]],[" ","to"," ","anybody"," ","else",". ","It"," ","almost"," ","makes"," ","me"," ","cry"," ","to"," ","think",[1,3,5,7,9,11,13,15,17,19]],["BH",32184,32189],[3,2,6]],
-            [["used"," ","ardently"," ","to"," ","hope"," ","that"," ","I"," ","might"," ","have"," ","a"," ","better"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",";",[0]],[" ","and"," ","I"," ","talked"," ","it"," ","over"," ","very"," ","often"," ","with"," ","the"," ","dear",[1,3,5,7,9,11,13,15,17,19]],["BH",33835,33840],[3,3,20]],
-            [["she"," ","looked"," ","at"," ","me",", ","and"," ","laid"," ","it"," ","on"," ","my"," ","fluttering"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",".",[0]],[" ","She"," ","raised"," ","me",", ","sat"," ","in"," ","her"," ","chair",", ","and"," ","standing"," ","me",[1,3,5,7,9,11,13,15,17,19]],["BH",37435,37440],[3,14,58]],
-            [["I"," ","had"," ","brought"," ","no"," ","joy"," ","at"," ","any"," ","time"," ","to"," ","anybody's"," ",[0,2,4,6,8,10,12,14,16,18]],["heart",[0]],[" ","and"," ","that"," ","I"," ","was"," ","to"," ","no"," ","one"," ","upon"," ","earth"," ","what",[1,3,5,7,9,11,13,15,17,19]],["BH",38895,38900],[3,16,69]],
-        ]
-        out = list(self._compute_path(data=data, path=[
+        out = list(self._compute_path(data=self.conc_data, path=[
             { "algorithm_name": "KWIC Grouper Ranker", "count_types": "on", "search_term": "the", "tokens_attribute": "word", "window_end": "10", "window_start": "-10"},
         ]))
         self.assertEqual(out, [
