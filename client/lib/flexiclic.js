719e494839286ea7d6b77bfb57a4f60cb35fd4a6
Jamie Lentin <jm@lentin.co.uk>
2025-03-12 10:51:38 +0000

    client/flexiclic: Use iframe parent's worker, if available
    Store the worker on the window object, if we have a parent, look for it
    there.

--- client/lib/flexiclic.js
+++ client/lib/flexiclic.js
@@ -7,9 +7,9 @@ var DisplayError = require('./alerts.js').prototype.DisplayError;
 module.exports.flexiclic = new Proxy({}, { get: function (target, propName) {
     if (propName === "shutdown") {
         return function () {
-            if (target._worker) {
-                target._worker.terminate();
-                target._worker = undefined;
+            if (window.flexiclic_worker) {
+                window.flexiclic_worker.terminate();
+                window.flexiclic_worker = undefined;
             }
             return Promise.resolve();
         };
@@ -18,45 +18,50 @@ module.exports.flexiclic = new Proxy({}, { get: function (target, propName) {
     // Assume any get is fetching a method, construct closure in response
     return function (kwargs) {
         return new Promise(function (resolve, reject) {
-            var transactionId;
+            var transactionId, worker;
 
-            // If not present, start webworker & listen to transactions
-            if (!target._worker) {
-                target._transactions = {};
-                target._worker = new Worker("/flexiclic/flexiclicWorker.js");
-                target._worker.addEventListener("message", function (e) {
+            if (window.parent && window.parent.flexiclic_worker) {
+                worker = window.parent.flexiclic_worker;
+            } else if (window.flexiclic_worker) {
+                worker = window.flexiclic_worker;
+            } else {
+                // If not present, start webworker & listen to transactions
+                window.flexiclic_worker = new Worker("/flexiclic/flexiclicWorker.js");
+                window.flexiclic_worker._fc_transactions = {};
+                worker = window.flexiclic_worker;
+                worker.addEventListener("message", function (e) {
                     var err;
                     if (!e.data.tx) {
                         throw new Error("Message has no transaction ID " + e.data);
                     }
-                    if (!target._transactions[e.data.tx]) {
+                    if (!worker._fc_transactions[e.data.tx]) {
                         throw new Error("No promise for transaction ID " + e.data.tx);
                     }
                     if (e.data.error) {
                         err = new DisplayError(e.data.error);
                         err.level = e.data.level || "error";
-                        target._transactions[e.data.tx].reject(err);
-                        delete target._transactions[e.data.tx];
+                        worker._fc_transactions[e.data.tx].reject(err);
+                        delete worker._fc_transactions[e.data.tx];
                     } else if (e.data.done === false) {
                         // Generator, collate parts into an array
-                        target._transactions[e.data.tx].rv = target._transactions[e.data.tx].rv || [];
-                        target._transactions[e.data.tx].rv.push(e.data.rv);
-                    } else if (target._transactions[e.data.tx].rv) {
+                        worker._fc_transactions[e.data.tx].rv = worker._fc_transactions[e.data.tx].rv || [];
+                        worker._fc_transactions[e.data.tx].rv.push(e.data.rv);
+                    } else if (worker._fc_transactions[e.data.tx].rv) {
                         // Return results of generator
-                        target._transactions[e.data.tx].resolve(target._transactions[e.data.tx].rv);
-                        delete target._transactions[e.data.tx];
+                        worker._fc_transactions[e.data.tx].resolve(worker._fc_transactions[e.data.tx].rv);
+                        delete worker._fc_transactions[e.data.tx];
                     } else {
                         // Call relevant resolve, throw away
-                        target._transactions[e.data.tx].resolve(e.data.rv);
-                        delete target._transactions[e.data.tx];
+                        worker._fc_transactions[e.data.tx].resolve(e.data.rv);
+                        delete worker._fc_transactions[e.data.tx];
                     }
                 });
             }
 
             // Send method call as request
             transactionId = crypto.randomUUID();
-            target._transactions[transactionId] = {resolve: resolve, reject: reject};
-            target._worker.postMessage({
+            worker._fc_transactions[transactionId] = {resolve: resolve, reject: reject};
+            worker.postMessage({
                 tx: transactionId,
                 method: propName.toString(),
                 kwargs: kwargs || {},
